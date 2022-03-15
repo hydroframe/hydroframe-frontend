96666472dc138ff559f6da9bb6123ef636877650
Rizbanul-BSU <90339383+Rizbanul-BSU@users.noreply.github.com>
2022-03-15 13:45:24 -0600

    adding all files and dependencies (#28)
    Co-authored-by: Rizbanul Hasan <rizbanulhasan@ENG401799.ht.home>

---
+++ pages/modeling.js
@@ -0,0 +1,49 @@
+import Button_modelling from '../components/Button_modelling';
+import { Grid } from "@material-ui/core";
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Link from "next/link"
+import React, {useState} from 'react';
+
+const modeling = () => {
+    const [curState, newState] = useState([
+        {
+            key: 1,
+            text: "My Domain 1"
+        },
+        {
+            key: 2,
+            text: "My Domain 2"
+        },
+        {
+            key: 3,
+            text: "My Domain 3"
+        },
+
+    ]);
+    
+    const onDelete = index => {
+        const temp = [...curState]; 
+        temp.splice(index, 1);
+        newState(temp); 
+    }
+    return (
+        <>
+        <Grid item xs={12} style={{margin: "5%"}}>
+        <Link href="/domain">
+        <Button variant="contained" style ={{marginBottom: "3%"}} startIcon={<AddIcon />}>Create New Domain</Button>
+        </Link>
+        {curState.map((todo, index) => (
+            <Button_modelling  index={index} todo={todo} onDelete={onDelete}/>
+        ))}
+        </Grid>
+
+        <Link href="/">
+        <Button variant="text" style ={{marginBottom: "3%"}} startIcon={<ArrowBackIcon />}>Landing Page</Button>
+        </Link>
+        </>
+    )
+}
+
+export default modeling
