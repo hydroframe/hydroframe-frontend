2f1573fe2ca36bb06b1a936cd4495ca558cdb1db
Rizbanul Hasan <rizbanulhasan@ENG401799.ht.home>
2022-04-05 11:41:13 -0600

    edited modeling page and added domain details page

--- components/Button_modelling.js
+++ components/Button_modelling.js
@@ -2,12 +2,15 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { Grid } from "@material-ui/core";
 import DeleteIcon from '@mui/icons-material/Delete';
+import Link from "next/link"
 
 const Button_modelling = ({todo, index, onDelete}) => {
     return (
         <Stack spacing={3} direction="row" style={{margin: "10px"}}>
-        <p style={{fontSize: "16px", marginRight: "5px"}}>{todo.text}</p>
-        <Button variant="contained">Domain Dashboard</Button>
+        <Link href="/domainDisplay"> 
+        <p style={{fontSize: "16px", marginRight: "5px", cursor:"pointer"}}>{todo.text}</p>
+        </Link>
+        {/* <Button variant="contained">Domain Dashboard</Button>  */}
         <Button variant="contained" color="error" startIcon={<DeleteIcon />} onClick={() => onDelete(index)}>Remove</Button>
       </Stack>
     )
