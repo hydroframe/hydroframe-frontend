3116f907455ee5aa48a80175c62d91dc476fe4fd
Rizbanul-BSU <90339383+Rizbanul-BSU@users.noreply.github.com>
2022-04-19 11:00:14 -0600

    Adding files for modeling and modeling dashboard page #HYF34 #HYF56 (#29)
    * edited modeling page and added domain details page
    
    * fixing file name and formating files
    
    * changed file name
    
    Co-authored-by: Rizbanul Hasan <rizbanulhasan@ENG401799.ht.home>
    Co-authored-by: Cristina Luis <boojums@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,31 +5,27 @@ import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <Grid
-      container
-      spacing={4}
-      justifyContent="center"
-    >
-      <Grid item xs={12}>
-        <h1> Welcome to HydroFrame</h1>
-        <p>Here you can build and run your own model,
-          explore data from CONUS simulations, and try the sandtank app.</p>
-      </Grid>
-      <Grid item xs={12} sm={6} md={5}>
-        <Link href="/modeling">
-          <a><Card title={"Model Builder"} /></a>
-        </Link>
-      </Grid>
-      <Grid item xs={12} sm={6} md={5}>
-        <Card title={"Data Explorer"} />
-      </Grid>
-      <Grid item xs={12} sm={6} md={5}>
-        <Card title={"Open SandTask App"} />
-      </Grid>
-      <Grid item xs={12} sm={6} md={5}>
-        <Card title={"Anything Else"} />
-      </Grid>
+    <Grid container spacing={4} justifyContent="center" align = "center">
+    <Grid item xs={12}>
+    <h1> Welcome to HydroFrame</h1>
+    <p>Here you can build and run your own model, 
+        explore data from CONUS simulations, and try the sandtank app.</p>
     </Grid>
-  )
+    <Grid item xs={12} sm={6} md={5}>
+        <Link href="/modeling">
+        <a><Card title={"Model Builder"} /></a>
+        </Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={5}>
+      <Card title={"Data Explorer"} />
+    </Grid>
+    <Grid item xs={12} sm={6} md={5}>
+      <Card title={"Open SandTask App"} />
+    </Grid>
+    <Grid item xs={12} sm={6} md={5}>
+      <Card title={"Anything Else"} />
+    </Grid>
+  </Grid>
+)
 }
 export default Home
