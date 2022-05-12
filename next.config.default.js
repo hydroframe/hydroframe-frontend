4373749158a225aa0b3161532a5de774d8cbf4be
Calla Chennault <callachennault@gmail.com>
2022-05-12 11:54:37 -0400

    add support for api url rewrites:

--- next.config.default.js
+++ next.config.default.js
@@ -4,9 +4,18 @@ const isProd = process.env.DEPLOY_STAGE === 'production'
 
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: isProd ? 'https://verde.princeton.edu/pun/sys/pfclm-flask' : 'http://127.0.0.1:5000',
+    trailingSlash: true,
     basePath: isProd ? '/pun/sys/pfclm-flask' : '',
-    trailingSlash: true
+    assetPrefix: isProd ? 'https://verde.princeton.edu/pun/sys/pfclm-flask' : '',
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: isProd ? 'https://verde.princeton.edu/pun/sys/pfclm-flask/api' : 'http://127.0.0.1:5000/api',
+                basePath: false,
+            }
+        ]
+    }
 };
 
 module.exports = nextConfig;
