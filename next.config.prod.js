8bfd8bd8272f08d38f460abcfc1c33e6f0ce4b96
Calla Chennault <callachennault@gmail.com>
2022-06-14 15:25:14 -0400

    Changes to config files

--- next.config.prod.js
+++ next.config.prod.js
@@ -5,15 +5,15 @@ const nextConfig = {
     trailingSlash: true,
     basePath: '/pun/sys/pfclm-flask',
     assetPrefix: 'https://verde.princeton.edu/pun/sys/pfclm-flask',
-    async rewrites() {
+    /*async rewrites() {
         return [
             {
                 source: '/api/:path*',
-                destination: 'https://verde.princeton.edu/pun/sys/pfclm-flask/api',
+                destination: 'https://verde.princeton.edu/pun/sys/pfclm-flask/api/:path*',
                 basePath: false,
             }
         ]
-    },
+    },*/
 };
 
 module.exports = nextConfig;
