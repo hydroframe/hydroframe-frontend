75862e1fecbef9ec460afdeca1277f4575306ec2
Calla Chennault <callachennault@gmail.com>
2022-08-24 16:56:02 -0400

    basepath correction

--- next.config.prod.js
+++ next.config.prod.js
@@ -3,7 +3,7 @@
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
-    basePath: '/pun/sys/app3',
+    basePath: '/pun/sys/pfclm-flask',
     assetPrefix: 'https://verde.princeton.edu/pun/sys/pfclm-flask',
     env: {
         basePath: 'https://verde.princeton.edu/pun/sys/pfclm-flask'
