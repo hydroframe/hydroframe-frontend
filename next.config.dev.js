523218a1e9df93be2294f8b6f50fd09d49dcbf00
Calla Chennault <callachennault@gmail.com>
2022-08-25 17:24:07 -0400

    Fix dev config and get request url

--- next.config.dev.js
+++ next.config.dev.js
@@ -3,16 +3,8 @@
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
-    basePath: '',
-    assetPrefix: '',
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://127.0.0.1:5000/api/:path*',
-                basePath: false,
-            }
-        ]
+    env: {
+        basePath: 'http://127.0.0.1:5000'
     },
 };
 
